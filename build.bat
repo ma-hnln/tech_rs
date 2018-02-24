e5a9821035f7109654644073f8b85edcd7cc7659
ma-hnln <marcel.hnln@gmail.com>
2018-02-24 18:22:32 +0100

    Reworked the algorithm section and added a CGRA section

--- build.bat
+++ build.bat
@@ -5,11 +5,11 @@ rem Run Latex to generate the .aux file required for bibtex
 pdflatex.exe -output-directory out main.tex
 
 rem Run bibtex to generate bib info for Latex
-copy bib.bib out
-cd out
-bibtex.exe main.aux
-cd ..\
+rem copy bib.bib out
+rem cd out
+rem bibtex.exe main.aux
+rem cd ..\
 
 rem Rerun Latex to include the generated bib info
-pdflatex.exe -output-directory out main.tex
+rem pdflatex.exe -output-directory out main.tex
 texworks.exe out\main.pdf
